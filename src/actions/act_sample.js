f7db0f5480d3e1959e3bb068351c3b4f36396b87
DurgaRaju <durgaraju@DurgaRajus-MacBook-Air.local>
2020-03-28 18:10:08 +0530

    last commit of the day

--- src/actions/act_sample.js
+++ src/actions/act_sample.js
@@ -3,7 +3,6 @@ export const SIMPLE_ACTION_TYPES = {
 }
 
 export const simpleAction = () =>{
-    console.log('call')
     return{
         type:SIMPLE_ACTION_TYPES.SIMPLE_ACTION,
         payload:'I am payload from simple action'
